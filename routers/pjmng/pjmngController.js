72b0ac42786f338de6464ca0614f38f86af2a1cb
cho jae kwon <jjg2362@naver.com>
2020-07-21 20:34:30 +0900

    make DGU532 page without download file function

--- routers/pjmng/pjmngController.js
+++ routers/pjmng/pjmngController.js
@@ -42,7 +42,6 @@ exports.getMyproject = (req, res) => {
     // console.log(query);
     connection.query(query, (error, results, fields) => {
       connection.release();
-
       if (error) {
         //throw error;
         console.error("query error : " + error);
@@ -493,39 +492,18 @@ exports.getSearchproject2 = (req, res) => {
   });
 };
 exports.getSearchproject3 = (req, res) => {
-  //session check
   if (!req.session.userId) {
     console.log("do not have a session.");
-    res.redirect("/");
-    return;
-  } else if (req.session.userType != "admin") {
-    console.log("do not match admin type.");
     res.redirect("/");
     return;
   } else {
     logger.putLog(req);
   }
-
-  //use connection
   var query =
-    "select b.class_type, d.class_num, b.team_id, d.team_name, a.std_id , d.leader_id, a.std_grade , a.std_name , a.major ,   ";
-  query += "a.email_ad ,a.phone_num , b.std_resume , ";
+    "select f.*,t.*, pp.prj_plan_report from final_product as f, team as t, project_plan_report as pp ";
   query +=
-    "e.prj_name as pc1_name, f.prj_name as pc2_name, g.prj_name as pc3_name , d.mentor_id , p.prj_name ";
-  query += "from student as a ";
-  query +=
-    "left join std_team_info as b on a.std_id = b.std_id and a.std_id = b.leader_id ";
-  query += "left join project_cart as c on b.team_id= c.team_id  ";
-  query += "left join team as d on d.team_id = b.team_id ";
-  query += "left join project as p on p.prj_id=d.prj_id ";
-  query +=
-    "left join (select prj_id,prj_name from project) as e on e.prj_id=c.project1 ";
-  query +=
-    "left join (select prj_id,prj_name from project) as f on f.prj_id=c.project2 ";
-  query +=
-    "left join (select prj_id,prj_name from project) as g on g.prj_id=c.project3 where d.use_yn=1 order by b.class_type, b.team_id desc;";
+    " where t.team_id = f.team_id and pp.team_id = t.team_id and t.use_yn = 1;";
 
-  //get connection from pool
   mysqlPool.pool.getConnection((err, connection) => {
     if (err) {
       //throw err;
@@ -536,6 +514,8 @@ exports.getSearchproject3 = (req, res) => {
     connection.query(query, (error, results, fields) => {
       connection.release();
 
+      console.log(results[0]);
+
       if (error) {
         //throw error;
         console.error("query error : " + error);
@@ -543,9 +523,8 @@ exports.getSearchproject3 = (req, res) => {
       }
 
       //use results and fields
-      // console.log('lookup all team.');
       res.render("pjmng/DGU532", {
-        teamInfo: results,
+        FinalLists: results,
         userInfo: req.session.userInfo,
       });
     });
