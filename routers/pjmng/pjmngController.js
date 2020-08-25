0ca6991a36d42bc009b1da12b54d0f0a3cb2ce10
JKCho <wornjs2362@gmail.com>
2020-08-25 16:33:51 +0900

    redirect the way when delete mentoring report file in DGU521/page

--- routers/pjmng/pjmngController.js
+++ routers/pjmng/pjmngController.js
@@ -874,7 +874,7 @@ exports.postMentoringReport1 = (req, res) => {
         }
 
         logger.putLogDetail(req, "Mentoring Report Deleted.");
-        var way = "/pjmng/DGU511/" + req.body.PJId;
+        var way = "/pjmng/DGU521/page/" + req.body.PJId;
         res.redirect(way);
       });
     });
@@ -1059,7 +1059,7 @@ exports.postMentoringReport1 = (req, res) => {
         }
 
         logger.putLogDetail(req, "Mentoring Report Updated.");
-        var way = "/pjmng/DGU511/" + req.body.PJId;
+        var way = "/pjmng/DGU521/page/" + req.body.PJId;
         res.redirect(way);
       });
     });
